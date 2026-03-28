2a4eb11666ecabc58c34b75588d6277aeafbb2e8
Timothy Davis <me@tmthy.dev>
2026-03-29 01:34:13 +1000

    Use CDN for KaTeX css

--- src/routes/(app)/sketches/[slug]/+page.svelte
+++ src/routes/(app)/sketches/[slug]/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-	import 'katex/dist/katex.min.css'
 	import '$lib/css/prism-gruvbox-dark.css'
 	import PostLayout from '$lib/PostLayout.svelte'
 
 	let { data } = $props()
 </script>
+
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.44/dist/katex.min.css" integrity="sha384-irXK0JiCGinqGL+slwVklbhJetrjczNwaP2lANewD8lKAs9n61SbQ3As28iSqXUE" crossorigin="anonymous">
+</svelte:head>
 
 <PostLayout>
 	<data.body />
