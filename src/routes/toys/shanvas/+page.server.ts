9db6c23a9c2b2695dc7134e4b1bed95efb352ea9
Timothy Davis <me@tmthy.dev>
2026-03-31 15:52:39 +1000

    Whoops

--- src/routes/toys/shanvas/+page.server.ts
+++ src/routes/toys/shanvas/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types'
-import { SHANVAS_SECRET_KEY } from '$env/static/private'
+import { SHANVAS_SECRET_KEY } from '$env/dynamic/private'
 import { error } from '@sveltejs/kit'
 
 export const prerender = false
