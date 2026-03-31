89e0669507077a960e5a08e2b57a5b9ab109d10d
Timothy Davis <me@tmthy.dev>
2026-03-31 15:37:10 +1000

    Load env for config properly

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
-	const dev = mode !== 'production'
+	const env = loadEnv(mode, process.cwd(), '')
+
 	return {
 		plugins: [tailwindcss(), sveltekit()],
 		server: {
 			proxy: {
 				'/api/shanvas': {
-					target: dev ? 'http://localhost:5678' : import.meta.env.SHANVAS_API_HOST,
+					target: env.SHANVAS_API_HOST,
 					changeOrigin: true,
 					rewrite: (path) => path.replace(/^\/api\/shanvas/, '')
 				}
