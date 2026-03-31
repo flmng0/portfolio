d835a9b7008d161a519453f0838c042ec36b8496
Timothy Davis <me@tmthy.dev>
2026-03-31 18:00:08 +1000

    Guard to error cleaner in prod

--- src/routes/toys/shanvas/consts.ts
+++ src/routes/toys/shanvas/consts.ts
@@ -1,4 +1,6 @@
+import { dev } from '$app/environment'
 import { env } from '$env/dynamic/public'
+import { error } from '@sveltejs/kit'
 
 export function apiPath(path: string) {
 	if (env.PUBLIC_SHANVAS_API_HOST) {
@@ -6,6 +8,10 @@ export function apiPath(path: string) {
 		return new URL(path, env.PUBLIC_SHANVAS_API_HOST)
 	}
 
-	console.log('Using proxy! (/api/shanvas)')
-	return '/api/shanvas' + path
+	if (dev) {
+		console.log('Using proxy! (/api/shanvas)')
+		return '/api/shanvas' + path
+	}
+
+	throw error(500, 'No shanvas host defined, and not in dev!')
 }
