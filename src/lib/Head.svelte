4537f92a46cca5e6d3845aa8fbe6adf7346c8ec2
Timothy Davis <me@tmthy.dev>
2026-04-10 22:52:44 +1000

    Whoops. meta[name] -> meta[property]

--- src/lib/Head.svelte
+++ src/lib/Head.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { site } from './consts'
 	import { page } from '$app/state'
-	let { title, description, image = '/fallback-banner.png' } = $props()
+	let { title, description, image = '/default-banner.png', type = 'website' } = $props()
 
 	let imagePath = $derived(new URL(image, site).toString())
 	let url = $derived(new URL(page.url.pathname, site).toString())
@@ -11,9 +11,14 @@
 	<title>{title}</title>
 	<meta name="description" content={description} />
 
-	<meta name="og:type" content="website" />
-	<meta name="og:title" content={title} />
-	<meta name="og:url" content={url} />
-	<meta name="og:description" content={description} />
-	<meta name="og:image" content={imagePath} />
+	<meta property="og:type" content={type} />
+	<meta property="og:title" content={title} />
+	<meta property="og:url" content={url} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={imagePath} />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+
+	<!-- No Twitter because X removed the documentation -->
+	<!-- Non developer supportive platforms don't get support from me *huff* -->
 </svelte:head>
